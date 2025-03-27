815371f22aa68a9f054a9705a631c169c9134b72
birbothedev <123966430+birbothedev@users.noreply.github.com>
2025-03-27 12:16:57 -0400

    added starter files and skeleton class for changeStack

---
+++ data proj 3/proj3/ChangeStack.java
@@ -0,0 +1,24 @@
+public class ChangeStack {
+    public ChangeStack(){
+
+    }
+
+    public void push(){
+
+    }
+    public int pop(){
+        return 1;
+    }
+
+    public int peekTop(){
+        return 1;
+    }
+
+    public void increaseValues(){
+
+    }
+
+    public void decreaseValues(){
+
+    }
+}
